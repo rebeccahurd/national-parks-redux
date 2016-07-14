c7fa1eb630d9465fe97b51c7e967e7ebad9acc67
rebeccahurd <rhhurd@gmail.com>
2016-07-14 14:11:46 -0400

    switching back

--- src/main/java/com/techelevator/model/jdbc/JDBCReservationDAO.java
+++ src/main/java/com/techelevator/model/jdbc/JDBCReservationDAO.java
@@ -1,11 +1,15 @@
 package com.techelevator.model.jdbc;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.model.Reservation;
 import com.techelevator.model.ReservationDAO;
@@ -18,23 +22,96 @@ private JdbcTemplate jdbcTemplate;
 	public JDBCReservationDAO(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	
-	@Override
-	public List<Reservation> getReservationsBySiteId(int siteId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public Reservation getReservationById(int reservationId) {
-		// TODO Auto-generated method stub
-		return null;
+		Reservation r = new Reservation();
+		String sqlGetReservationById = "SELECT * " +
+									   "FROM reservation " +
+									   "WHERE reservation_id = ?";
+		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetReservationById, reservationId);
+		if (result.next()) {
+			r.setReservationId(result.getInt("reservation_id"));
+			r.setSiteId(result.getInt("site_id"));
+			r.setName(result.getString("name"));
+			
+			Date fd = result.getDate("from_date");
+			LocalDate fromDate = fd.toLocalDate();
+			r.setFromDate(fromDate);
+			
+			Date td = result.getDate("to_date");
+			LocalDate toDate = td.toLocalDate();
+			r.setToDate(toDate);
+			
+			Date cd = result.getDate("create_date");
+			LocalDate createDate = cd.toLocalDate();
+			r.setCreateDate(createDate);
+		}
+		return r;
+	}
+	
+	@Override
+	public List<Reservation> getReservationsBySiteId(int siteId) {
+		ArrayList<Reservation> reservationList = new ArrayList<>();
+		String sqlGetReservationsBySiteId = "SELECT * " +
+									   "FROM reservation " +
+									   "WHERE site_id = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetReservationsBySiteId, siteId);
+		while (results.next()) {
+			Reservation r = new Reservation();
+			r.setReservationId(results.getInt("reservation_id"));
+			r.setSiteId(results.getInt("site_id"));
+			r.setName(results.getString("name"));
+			
+			Date fd = results.getDate("from_date");
+			LocalDate fromDate = fd.toLocalDate();
+			r.setFromDate(fromDate);
+			
+			Date td = results.getDate("to_date");
+			LocalDate toDate = td.toLocalDate();
+			r.setToDate(toDate);
+			
+			Date cd = results.getDate("create_date");
+			LocalDate createDate = cd.toLocalDate();
+			r.setCreateDate(createDate);
+			
+			reservationList.add(r);
+		}
+		return reservationList;
 	}
 
 	@Override
 	public void saveReservation(Reservation r) {
-		// TODO Auto-generated method stub
+		String sqlInsertReservation = "INSERT INTO reservation(reservation_id, site_id, name, from_date, to_date) " +
+									  "VALUES(?, ?, ?, ?, ?)";
+		jdbcTemplate.update(sqlInsertReservation, r.getReservationId(),
+													r.getSiteId(),
+													r.getName(),
+													r.getFromDate(),
+													r.getToDate());
 		
 	}
-
+	
+	private Reservation mapRowToReservation(SqlRowSet results) {
+		Reservation r;
+		
+		r = new Reservation();
+		r.setReservationId(results.getInt("reservation_id"));
+		r.setSiteId(results.getInt("site_id"));
+		r.setName(results.getString("name"));
+		
+		Date fd = results.getDate("from_date");
+		LocalDate fromDate = fd.toLocalDate();
+		r.setFromDate(fromDate);
+		
+		Date td = results.getDate("to_date");
+		LocalDate toDate = td.toLocalDate();
+		r.setToDate(toDate);
+		
+		Date cd = results.getDate("create_date");
+		LocalDate createDate = cd.toLocalDate();
+		r.setCreateDate(createDate);
+		
+		return r;
+	}
 }
--- src/test/java/com/techelevator/JDBCReservationDAOIntegrationTest.java
+++ src/test/java/com/techelevator/JDBCReservationDAOIntegrationTest.java
@@ -1,14 +1,23 @@
 package com.techelevator;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
+import com.techelevator.model.Reservation;
 import com.techelevator.model.jdbc.JDBCReservationDAO;
 
 public class JDBCReservationDAOIntegrationTest {
@@ -33,25 +42,33 @@ public class JDBCReservationDAOIntegrationTest {
 	
 	@Before
 	public void setup() {
-		String sqlInsertTestReservation1 = "INSERT INTO reservation(reservation_id, site_id, name, from_date, to_date, create_date) "
-											+"VALUES(100, 100, 'Rebecca', '2016-07-01', '2016-07-08'";
-		String sqlInsertTestReservation2 = "INSERT INTO reservation(reservation_id, site_id, name, from_date, to_date, create_date) "
-											+"VALUES(101, 100, 'Dave', '2016-07-09', '2016-07-15'";
-		String sqlInsertTestReservation3 = "INSERT INTO reservation(reservation_id, site_id, name, from_date, to_date, create_date) "
-											+"VALUES(102, 200, 'Anthony', '2016-07-09', '2016-07-15'";
+		String sqlInsertTestPark = "INSERT INTO park (park_id, name, location, establish_date, area, visitors, description) "
+									+ "VALUES (300, 'Tech Elevator Peaks', 'Ohio', '1933-02-26', 12345, 55000, "
+									+ "'Very pretty park.')";
 		
+		String sqlInsertTestCampground1 = "INSERT INTO campground (campground_id, park_id, name, open_from_mm, open_to_mm, daily_fee) "
+											+ "VALUES (500, 300, 'SampleCampground1', '01', '12', 35.00)";
+
 		String sqlInsertTestSite1 = "INSERT INTO site(site_id, campground_id, site_number) "
-											+"VALUES(100, 5, 10)";
+									+"VALUES(1000, 500, 10)";
 		String sqlInsertTestSite2 = "INSERT INTO site(site_id, campground_id, site_number) "
-											+"VALUES(101, 6, 10)";
+									+"VALUES(2000, 500, 10)";
+
+		String sqlInsertTestReservation1 = "INSERT INTO reservation(reservation_id, site_id, name, from_date, to_date) "
+											+"VALUES(100, 1000, 'Rebecca', '2016-07-01', '2016-07-08')";
+		String sqlInsertTestReservation2 = "INSERT INTO reservation(reservation_id, site_id, name, from_date, to_date) "
+											+"VALUES(101, 1000, 'Dave', '2016-07-09', '2016-07-15')";
+		String sqlInsertTestReservation3 = "INSERT INTO reservation(reservation_id, site_id, name, from_date, to_date) "
+											+"VALUES(102, 2000, 'Anthony', '2016-07-09', '2016-07-15')";
 		
 		jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.update(sqlInsertTestPark);
+		jdbcTemplate.update(sqlInsertTestCampground1);
+		jdbcTemplate.update(sqlInsertTestSite1);
+		jdbcTemplate.update(sqlInsertTestSite2);
 		jdbcTemplate.update(sqlInsertTestReservation1);
 		jdbcTemplate.update(sqlInsertTestReservation2);
 		jdbcTemplate.update(sqlInsertTestReservation3);
-		jdbcTemplate.update(sqlInsertTestSite1);
-		jdbcTemplate.update(sqlInsertTestSite2);
-		
 		
 		reservationDAO = new JDBCReservationDAO(dataSource);
 	}
@@ -61,6 +78,49 @@ public class JDBCReservationDAOIntegrationTest {
 		dataSource.getConnection().rollback();
 	}
 	
+	@Test 
+	public void returns_reservation_by_id() throws SQLException {
+		Reservation r1 = reservationDAO.getReservationById(100);
+		Reservation r2 = reservationDAO.getReservationById(101);
+		
+		assertNotNull(r1);
+		assertEquals("Dave",r2.getName());
+	}
+	
+	@Test 
+	public void returns_reservations_by_site_id() throws SQLException {
+		List<Reservation> testReservations1 = reservationDAO.getReservationsBySiteId(2000);
+		assertNotNull(testReservations1);
+		assertEquals(1, testReservations1.size());
+		
+		List<Reservation> testReservations2 = reservationDAO.getReservationsBySiteId(1000);
+		assertEquals(2, testReservations2.size());
+	}
+	
+	@Test 
+	public void save_reservation_and_read_it_back() throws SQLException {
+		Reservation r = new Reservation();
+		
+		r.setReservationId(5000);
+		r.setSiteId(1000);
+		r.setName("David");
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate fromDate = LocalDate.parse("2016-07-01", formatter);
+		r.setFromDate(fromDate);
+		
+		LocalDate toDate = LocalDate.parse("2016-07-05", formatter);
+		r.setToDate(toDate);
+		
+		reservationDAO.saveReservation(r);
+		
+		assertEquals(1000, reservationDAO.getReservationById(5000).getSiteId());
+	}
 	
 }
+
+
+
+
+
 
